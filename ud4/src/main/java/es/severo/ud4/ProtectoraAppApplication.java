ba5a8b8060d87d5dd55553355ec73766c367d1f8
Octansito <octanecodlol@gmail.com>
2025-12-23 20:48:38 +0100

    Inicio Proyecto

---
+++ ud4/src/main/java/es/severo/ud4/ProtectoraAppApplication.java
@@ -0,0 +1,13 @@
+package es.severo.ud4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtectoraAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtectoraAppApplication.class, args);
+	}
+
+}
---
+++ ud4/src/test/java/es/severo/ud4/ProtectoraAppApplicationTests.java
@@ -0,0 +1,13 @@
+package es.severo.ud4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtectoraAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
